6e43178acdb10967abbae84799ca27539389dd64
Tathagata Hazra <tathagatahazra4@gmail.com>
2026-07-31 14:44:15 +0530

    Improve mobile responsive new layout

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-
 import Sidebar from "@/components/layout/Sidebar";
 import Navbar from "@/components/layout/Navbar";
 
@@ -13,25 +12,31 @@ export default function AppLayout({
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="flex min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-gray-100">
+      
+      {/* DESKTOP SIDEBAR - FIXED / STATIONARY */}
+      <div className="hidden lg:block fixed left-0 top-0 w-64 h-screen z-30">
+        <Sidebar open={true} onClose={() => setSidebarOpen(false)} />
+      </div>
 
-      <Sidebar
-        open={sidebarOpen}
-        onClose={() => setSidebarOpen(false)}
-      />
+      {/* MOBILE SIDEBAR - OVERLAY */}
+      <div className="lg:hidden">
+        <Sidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)} />
+      </div>
 
-      <div className="flex-1 flex flex-col">
+      {/* MAIN AREA - THIS SCROLLS, SIDEBAR DOESN'T */}
+      <div className="flex flex-col min-h-screen lg:ml-64">
+        
+        {/* NAVBAR - STICKY ON TOP */}
+        <div className="sticky top-0 z-20">
+          <Navbar onMenuClick={() => setSidebarOpen(true)} />
+        </div>
 
-        <Navbar
-          onMenuClick={() => setSidebarOpen(true)}
-        />
-
-        <main className="flex-1 p-4 md:p-6 overflow-auto">
+        <main className="flex-1 p-4 md:p-6">
           {children}
         </main>
 
       </div>
-
     </div>
   );
 }
